2c35d6656fddbb1053f6edb15818747cff66e2fc
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2022-09-22 11:18:21 +0100

    Deploy

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -33,4 +33,32 @@ const createApp = async (req, res) => {
     }
 }
 
-module.exports = {getApps, getApp, createApp}
+const updateApp = async (req, res) => {
+    const token = req.headers.authorization.split(' ')[1]
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    const {name, website, image, appFile, description, contactMail} = req.body
+    const user = await User.findById(decoded.userId)
+    try {
+        if(user.role !== 'admin') return res.status(401).json({ message: 'Unauthorized' })
+        const app = await App.findByIdAndUpdate(req.params.id, {name, website, image, appFile, description, contactMail}, {new: true})
+        return res.status(200).json({ app })
+    } catch (err) {
+        return res.status(500).json({ message: err.message })
+    }
+}
+
+const deleteApp = async (req, res) => {
+    const token = req.headers.authorization.split(' ')[1]
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    const user = await User.findById(decoded.userId)
+    try {
+        if(user.role !== 'admin') return res.status(401).json({ message: 'Unauthorized' })
+        const app = await App.findByIdAndDelete(req.params.id)
+        return res.status(200).json({ app })
+    } catch (err) {
+        return res.status(500).json({ message: err.message })
+    }
+}
+
+
+module.exports = {getApps, getApp, createApp, updateApp, deleteApp}
--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-const {getApps, getApp, createApp} = require('../controllers/AppController')
+const {getApps, getApp, createApp, updateApp, deleteApp} = require('../controllers/AppController')
 
 router.get('/apps', getApps)
 router.get('/app/:id', getApp)
 router.post('/app', createApp)
+router.patch('/app/:id', updateApp)
+router.delete('/app/:id', deleteApp)
+
 module.exports = router
